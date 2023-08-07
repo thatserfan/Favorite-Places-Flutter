a5fb9f7538c2070547a64fa6e4c4bbd12352d408
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-07 16:18:22 +0330

    Add image field in model

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:favorite_palces/providers/user_places.dart';
 import 'package:favorite_palces/widgets/image_input.dart';
 import 'package:flutter/material.dart';
@@ -12,15 +14,18 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePalce() {
     final enteredText = _titleController.text;
 
-    if (enteredText.trim().isEmpty) {
+    if (enteredText.trim().isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredText);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredText, _selectedImage!);
 
     Navigator.of(context).pop();
   }
@@ -51,7 +56,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               ),
             ),
             const SizedBox(height: 10),
-            ImageInput(),
+            ImageInput(onPickImage: (image) {
+              _selectedImage = image;
+            }),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePalce,
