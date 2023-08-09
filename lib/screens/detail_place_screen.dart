3bc8827c447c9f0ea072d47316bf7a3da4c508ab
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-09 11:42:02 +0330

    Add location pic and address on detail place screen

--- lib/screens/detail_place_screen.dart
+++ lib/screens/detail_place_screen.dart
@@ -6,6 +6,13 @@ class DetailPlaceScreen extends StatelessWidget {
 
   final Place place;
 
+   String get locationImage {
+
+    final lat = place.location.lat;
+    final lng = place.location.lng;
+    return 'https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/$lat,$lng/17?pp=$lat,$lng;37;&mapSize=600,300&key=AqzoIhkb99cryUre2QHyqdXEwblhBnBRPaI4rfTqwOucAtxFwxgt8kHVbeXhArrV';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +26,44 @@ class DetailPlaceScreen extends StatelessWidget {
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
-          )
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
