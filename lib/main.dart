9af6b6ae1a452df7ff8b9e7c7b548ce9f746d617
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-12 14:05:32 +0330

    Fix warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:favorite_palces/screens/map.dart';
 import 'package:favorite_palces/screens/places_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
