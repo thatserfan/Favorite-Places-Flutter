0f6a4cb0dcafd52db9517ff9cd9d31ac0b0a54f8
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-12 14:38:13 +0330

    Save on data on local database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 import 'package:favorite_palces/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,6 +16,24 @@ class UserPlacesNotifire extends StateNotifier<List<Place>> {
 
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+      },
+      version: 1,
+    );
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title' : newPlace.title,
+      'image' : newPlace.image.path,
+      'lat' : newPlace.location.lat,
+      'lng' : newPlace.location.lng,
+      'address' : newPlace.location.address,
+    });
 
     state = [newPlace, ...state];
   }
