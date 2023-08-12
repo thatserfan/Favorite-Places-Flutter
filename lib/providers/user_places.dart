ab8f08a9704e734f60cc3596541712403fdb1546
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-12 14:25:34 +0330

    Add image on local

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,13 +1,19 @@
 import 'dart:io';
-
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 import 'package:favorite_palces/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifire extends StateNotifier<List<Place>> {
   UserPlacesNotifire() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
 
     state = [newPlace, ...state];
   }
